7a7255f0c43a315aed6c9ba4a0b39d9782346921
Tuomo <tuomo.vidberg@gmail.com>
2024-05-31 11:14:41 +0300

    relative numbers and colorscheme

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -740,15 +740,30 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
+  {
+    'rose-pine/neovim',
+    priority = 1000,
+    name = 'rose-pine',
+    init = function()
+      vim.cmd.colorscheme 'rose-pine-moon'
+      vim.cmd.hi 'Comment gui=none'
+      require('rose-pine').setup {
+        variant = 'moon',
+        dark_variant = 'moon',
+      }
     end,
   },
 
